e7249415326f29a0aa9ae9f768bf37d0fda8a193
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-01 19:07:27 +0300

    add sor array function

--- index.js
+++ index.js
@@ -114,4 +114,9 @@ function netSalaryCalculator(salary) {
   };
   return parseFloat(salary - payeeTaxRates(salary) - nhifRates(salary) - nssfRates(salary) - housingLevy(salary)).toFixed(2);
 }
-console.log(netSalaryCalculator(15999))
+//console.log(netSalaryCalculator(15999))
+const input = [5, 6, 1, 3, 4, 2]
+function sortArray(input) {
+  return input.sort();
+}
+//console.log(sortArray(input))
