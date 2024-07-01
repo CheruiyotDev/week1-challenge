4e47f3ef72e352bc83879833f17fecfb5b1beeb5
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-01 19:34:51 +0300

    Volume of a cylinder function

--- index.js
+++ index.js
@@ -132,3 +132,14 @@ function stairCase() {
   return output;
 }
 //console.log(stairCase())
+
+ function volumeOfCylinder() {
+  const cylinder = {
+    radius: 5,
+    height: 10,
+    pi: 3.142
+  }
+  return cylinder.pi * (cylinder.radius ** 2) * cylinder.height
+ }
+
+ //console.log(volumeOfCylinder())
