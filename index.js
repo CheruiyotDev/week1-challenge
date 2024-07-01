e582b8e35858f2246e8fc4992ccb0a61d65bf609
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-01 19:26:36 +0300

    staircase using nested loop

--- index.js
+++ index.js
@@ -120,3 +120,15 @@ function sortArray(input) {
   return input.sort();
 }
 //console.log(sortArray(input))
+function stairCase() {
+  const numOfRows = 5;
+  let output = '';
+  for (let i = 1; i <= numOfRows; i++) {
+    for (let j = 1; j <= i; j++) {
+      output += '#';
+    }
+    output += '\n';
+  }
+  return output;
+}
+//console.log(stairCase())
