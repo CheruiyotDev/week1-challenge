51bc7ac144aa04148ffb5cbc59cc88ea2c1554d1
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-03 07:52:38 +0300

    Organized net salary functions and addede comments and fixed speed detector

--- net-salary/index.js
+++ net-salary/index.js
@@ -1,7 +1,7 @@
 //Calculate net salary
 function netSalaryCalculator(salary, benefits) {
-  const netSalary = parseFloat(salary - (payeeTaxRates(salary) + nhifRates(salary) + nssfRates(salary))).toFixed(2);
-  return netSalary + benefits;
+  const netSalary = parseFloat((salary + benefits) - (payeeTaxRates(salary) + nhifRates(salary) + nssfRates(salary))).toFixed(2);
+  return netSalary;
 }
 console.log(netSalaryCalculator(15999))
 // Calculate payee
